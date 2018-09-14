507091dc8c4b9579f2b3c68a4ba22655732fc6f6
vishang soni <vishangsoni@gmail.com>
2018-09-13 22:55:48 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Hi, Myself Vishang Soni, <br/>
+      A front end devloper living in Toronto Canada.
+      <br/>Currently, I am experimenting with React Native and looking for new opportunity in this area.</p>
+    <p>You can reach me out at vishangsoni@gmail.com</p>
+    <p>Link to my <a href="www.github.com/vish448">Github Repo</a></p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
