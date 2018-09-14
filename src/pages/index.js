a49d1326dff5e1e1815a8c7639dff31d8fc14b74
vishang soni <vishangsoni@gmail.com>
2018-09-13 23:23:56 -0400

    Updated git hub link

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
       A front end devloper living in Toronto Canada.
       <br/>Currently, I am experimenting with React Native and looking for new opportunity in this area.</p>
     <p>You can reach me out at vishangsoni@gmail.com</p>
-    <p>Link to my <a href="www.github.com/vish448">Github Repo</a></p>
+    <p>Link to my <a href="https://github.com/vish448" target="_blank">Github Repo</a></p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
